f674363273ec4f965a4f2b53e1bec9a5194f5c61
Daniel Yatali <danielyatali902@gmail.com>
2022-03-24 11:17:13 -0400

    Set up chat draft

---
+++ src/routes/login.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Login from '../components/loginForm.svelte';
+</script>
+
+<div class="form-container">
+	<Login />
+</div>
+
+<style>
+</style>
---
+++ src/routes/signup.svelte
@@ -0,0 +1,18 @@
+<script>
+	import SignUpForm from '../components/signupForm.svelte';
+</script>
+
+<div class="form-container">
+	<SignUpForm />
+</div>
+
+<svelte:head>
+	<style>
+		body {
+			background: black;
+		}
+	</style>
+</svelte:head>
+
+<style>
+</style>
