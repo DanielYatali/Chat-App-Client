236fd9fe02130ccd72e552f312c01368461b4b3e
Daniel Yatali <danielyatali902@gmail.com>
2022-04-16 20:55:48 -0400

    edit endpoints

---
+++ src/routes/home.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Nav from '../components/nav.svelte';
+</script>
+
+<div>
+	<ul />
+	<Nav />
+</div>
+<div />
+
+<svelte:head>
+	<style>
+		body {
+			background-image: url('https://i.imgur.com/0CUwW1S.jpeg');
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+	</style>
+</svelte:head>
