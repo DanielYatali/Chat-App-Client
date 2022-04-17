bc9fd6c94415a3d69b0ddcffb9e67c7ec4af0cfc
Daniel Yatali <danielyatali902@gmail.com>
2022-04-17 18:02:06 -0400

    styles lots of styles

--- src/components/search.svelte
+++ src/components/search.svelte
@@ -30,7 +30,7 @@
 													type="text"
 													name="company-website"
 													id="company-website"
-													class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+													class="input-gold flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
 													placeholder="www.example.com"
 												/>
 											</div>
